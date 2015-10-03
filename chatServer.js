06d222dbe08ae8fa6e1771f16c11673cee296d2e
root <root@localhost.localdomain>
2015-10-04 01:46:52 +0900

    Basic Chatting Programme

---
+++ chatServer.js
@@ -0,0 +1,25 @@
+var express = require('express');
+// var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+// app.use(express.static(path.join(_dirname,'public')));
+
+var httpServer = http.createServer(app).listen(4000,function(req,res) {
+  console.log('Socket IO server has been started');
+});
+
+var io = require('socket.io').listen(httpServer);
+
+io.sockets.on('connection',function(socket) {
+  socket.emit('toclient',{msg:'welcome!'});
+  socket.on('fromclient',function(data) {
+    socket.broadcast.emit('toclient',data);
+    socket.emit('toclient',data);
+    console.log('Message from client :'+data.msg);
+  })
+});
+
+
+
