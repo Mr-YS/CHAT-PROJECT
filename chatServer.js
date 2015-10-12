a4d0753c57e4e35160eb061544bcae5128ed4669
Mr-YS <lucky.yoon0819@gmail.com>
2015-10-12 21:13:48 +0900

    Err Record

--- chatServer.js
+++ chatServer.js
@@ -4,7 +4,7 @@ var http = require('http');
 var path = require('path');
 
 var app = express();
-// app.use(express.static(path.join(_dirname,'public')));
+app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/', function(req,res) {
   res.sendFile(path.join(__dirname + '/index.html'));
