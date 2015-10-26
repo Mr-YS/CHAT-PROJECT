81f0c2970172ecf9cae965b15982d6508cc5c952
Mr-YS <lucky.yoon0819@gmail.com>
2015-10-26 20:18:14 +0900

    .json added

--- chatServer.js
+++ chatServer.js
@@ -1,43 +1,25 @@
 var express = require('express');
-// var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 
-var cookieParser = require('cookie-parser')();
-var session = require('cookie-session')({secret:'secret' };
-
 var app = express();
 app.use(express.static(path.join(__dirname + '/public')));
-app.use(cookieParser);
-app.use(session);
 
-app.get('/', function(req,res) {
+app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-var httpServer = http.createServer(app).listen(4000,function(req,res) {
+var httpServer = http.createServer(app).listen(4000, function(req,res) {
   console.log('Socket IO server has been started');
 });
 
 var io = require('socket.io').listen(httpServer);
 
 io.sockets.on('connection',function(socket) {
-  socket.emit('toclient',{name:'ADMIN.Y$',msg:'welcome!'});
+  socket.emit('toclient',{msg:'welcome!'});
   socket.on('fromclient',function(data) {
     socket.broadcast.emit('toclient',data);
     socket.emit('toclient',data);
-    console.log('Message from client :'+data.name+' : '+data.msg);
+    console.log('Message from client : '+data.name+ ' : '+data.msg);
   })
-
-  io.use(function(socket, next) {
-    var req = socket.handshake;
-    var res = {};
-    cookieParser(req, res, function(err) {
-      if(err) return next(err) {
-        session(req, res, next);
-      });
-    });
 });
-
-
-
